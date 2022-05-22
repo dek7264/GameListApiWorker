aceadb27e5bff706a5b58b52dcd4b4d4584c64a9
Derek Frazier <ffxierdek@comcast.net>
2022-05-21 22:22:46 -0400

    Add solution from template

---
+++ GameListApiWorker/Program.cs
@@ -0,0 +1,10 @@
+using GameListApiWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
