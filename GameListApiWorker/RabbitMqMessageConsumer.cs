73b412fbddfa5e8355bd3d2f8cf4b9fb35731c2a
Derek Frazier <ffxierdek@comcast.net>
2022-06-18 23:08:05 -0400

    Update to use MassTransit
    Comment out RabbitMQ setup code

---
+++ GameListApiWorker/RabbitMqMessageConsumer.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using Newtonsoft.Json;
+
+namespace GameListApiWorker
+{
+    public record TestMessage(string TestString, int TestInt);
+
+    public class RabbitMqMessageConsumer : IConsumer<TestMessage>
+    {
+        public ILogger<RabbitMqMessageConsumer> Logger { get; }
+
+        public RabbitMqMessageConsumer(ILogger<RabbitMqMessageConsumer> logger)
+        {
+            Logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<TestMessage> context)
+        {
+            Logger.LogInformation("Message Consumed! Message: {message}", JsonConvert.SerializeObject(context.Message));
+            return Task.CompletedTask;
+        }
+    }
+}
--- GameListApiWorker/Worker.cs
+++ GameListApiWorker/Worker.cs
@@ -18,33 +18,38 @@ namespace GameListApiWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
-            //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            //    await Task.Delay(1000, stoppingToken);
-            //}
-            var factory = new ConnectionFactory()
-            {
-                HostName = Configuration["RabbitMqHostName"]
-            };
-            factory.DispatchConsumersAsync = true;
-            var queueName = "test-queue-local";
-            var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();
 
-            channel.QueueDeclare(queueName);
-
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.Received += async (ch, ea) =>
-            {
-                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-                //perform processing
-                Logger.LogInformation($"Message Recieved: {{messageBody}}", JsonConvert.SerializeObject(message));
-                channel.BasicAck(ea.DeliveryTag, false);
-                await Task.Yield();
-            };
-
-            channel.BasicConsume(queueName, false, consumer);
         }
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    //while (!stoppingToken.IsCancellationRequested)
+        //    //{
+        //    //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //    //    await Task.Delay(1000, stoppingToken);
+        //    //}
+        //    var factory = new ConnectionFactory()
+        //    {
+        //        HostName = Configuration["RabbitMqHostName"]
+        //    };
+        //    factory.DispatchConsumersAsync = true;
+        //    var queueName = "test-queue-local";
+        //    var connection = factory.CreateConnection();
+        //    var channel = connection.CreateModel();
+
+        //    channel.QueueDeclare(queueName);
+
+        //    var consumer = new AsyncEventingBasicConsumer(channel);
+        //    consumer.Received += async (ch, ea) =>
+        //    {
+        //        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+        //        //perform processing
+        //        Logger.LogInformation($"Message Recieved: {{messageBody}}", JsonConvert.SerializeObject(message));
+        //        channel.BasicAck(ea.DeliveryTag, false);
+        //        await Task.Yield();
+        //    };
+
+        //    channel.BasicConsume(queueName, false, consumer);
+        //}
     }
 }
